7fe5f2be14345829203d56799065aa67504f0d2b
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2022-09-03 22:45:26 -1000

    Issue-001: Navbar & Landing Page + About Us
    Practice making cards

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -17,13 +17,13 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg" style={menuStyle}>
       <Container>
-        <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/"><h1>meteor-application-template-production</h1></Navbar.Brand>
+        <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/"><h1>Alahele</h1></Navbar.Brand>
         <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
         <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
           <Nav className="me-auto justify-content-start">
             {currentUser ? ([
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
+              <Nav.Link id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} to="/add" key="add">User Page</Nav.Link>,
+              <Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">Bills</Nav.Link>,
             ]) : ''}
             {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
               [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin</Nav.Link>,
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Card } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
@@ -7,16 +7,86 @@ const Landing = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+        <Image roundedCircle src="https://www.hsta.org/wp-content/uploads/2020/11/img-DOE-logo-4x3-1.jpg" width="200px" />
       </Col>
 
       <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
+        <h1>Welcome to Alahele</h1>
+        <p>Let us help you on find your alahele through the legislative process.</p>
       </Col>
+    </Row>
+
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>About Us</Card.Title>
+                <Card.Text>
+                  <p> "Alahele": Route, Way. </p>
+
+                  Aloha our team is comprised of overworked and underpaid UH Manoa students. Please donate and we will be sure to provide you a 'alahele' to facilitate the legislative process.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+      ))}
+
+      {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Helpful Tips</Card.Title>
+                <Card.Text>
+                  <h6>Adding Bills To Your Workspace</h6>
+                  <p>Blah blah blah</p>
+                  <p>Blah blah blah</p>
+                  <p>Blah blah blah</p>
+                  <p>Picture Here</p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+      ))}
+
+      {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Users Questions & Answers</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+      ))}
+
+      {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Ask Our Team</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+      ))}
+
 
     </Row>
+
   </Container>
+
 );
 
+
+
 export default Landing;
+
